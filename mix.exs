26ab62f95fb08de5b3ec6e6c855889887549af62
Matt <2172212+TheEndIsNear@users.noreply.github.com>
2025-10-10 14:50:48 -0600

    add igniter to the project

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule NasaApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:igniter, "~> 0.6", only: [:dev, :test]},
       {:req, "~> 0.5.0"},
       {:dialyxir, "~> 1.4.4", only: :dev, runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
